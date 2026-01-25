c4527bf1ddc718555065de5b0bd5be03f3caa62d
299-Aurora-Project <shinichiro.kaku@myokanagan.bc.ca>
2026-01-25 12:33:51 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,9 @@
-@@ -1,15 +1,96 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting(); // 新しいSWをすぐに有効化
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // キャッシュ戦略が必要な場合はここに記述
-});
-
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.text() : '通知があります';
   event.waitUntil(
-    clients.openWindow('/')
+    self.registration.showNotification('PWA通知', {
+      body: data,
+      icon: 'icon.png'
+    })
   );
 });
